e59caadce2e2a6d8f1f459ed7e489d86fd5fdf00
Saji Nael Zeer <saji.nael.98@gmail.com>
2022-11-01 12:55:20 +0200

    Initial commit

---
+++ src/main/java/com/example/hookahappapi/HookahAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hookahappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookahAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HookahAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hookahappapi/HookahAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hookahappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookahAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
